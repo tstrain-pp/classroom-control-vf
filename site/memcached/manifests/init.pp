5152282bbe944bc8acc58c80a9f3d0136e57749d
tstrain <tstrain@planetpayment.com>
2016-05-24 15:48:00 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,1 +1,22 @@
+class memcached {
+  
+  package {'memcached':
+    ensure => 'present',
+  }
+  
+  file { '/etc/sysconfig/memcached':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0744',
+    source  => 'puppet:///modules/memcached/memcached',
+    require => Package['memcached'],
+  }
 
+  service {'memcached':
+    ensure => 'running',
+    enable => 'true',
+    subscribe => File['/etc/sysconfig/memcached'],
+  }
+
+ }   
