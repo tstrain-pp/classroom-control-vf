8c56e11e3e04ff0bc6cb18deb5db8f5ab1d1b9dc
tstrain <tstrain@planetpayment.com>
2016-05-24 12:02:40 -0400

    Create init.pp

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,9 @@
+class skeleton {
+  file {'/etc/skel':
+    ensure  => 'directory',
+  }
+  
+  file {'/etc/skel/.bashrc':
+    ensure  => 'file',
+    source  => 'puppet:///modules/skeleton/bashrc',
+  }
