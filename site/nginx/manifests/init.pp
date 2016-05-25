600f5009f391e12da205cd4f960533c66e92a738
tstrain <tstrain@planetpayment.com>
2016-05-25 09:28:38 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -4,6 +4,31 @@ class nginx {
     ensure => 'present',
   }
   
+  file { '/var/www':
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+
+  file { '/var/www/index.html':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    source  => 'puppet:///modules/nginx/index.html',
+  }
+  
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+  
   file { '/etc/nginx/nginx.conf':
     ensure  => 'file',
     owner   => 'root',
@@ -11,6 +36,13 @@ class nginx {
     mode    => '0644',
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/conf.d':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
   }
 
   service {'nginx':
