842f8981bf9c683da662fa23bcb42eb770ff7955
tstrain <tstrain@planetpayment.com>
2016-05-25 13:09:21 -0400

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,14 +1,13 @@
 user::managed_user(
   $group = $title,
   ) { 
-  
     user {$title:
         ensure => present,
     }
+    
     file {"/home/${title}":
       ensure => 'directory',
       owner  => $title,
       group  => $group,
     }
 }
-  
